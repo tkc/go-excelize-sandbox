77adfdc10d5f78e73b793f8f7aec763830c89814
tkc <takeshi.iijima.onga@gmail.com>
2020-04-06 20:04:14 +0900

    Add SAM

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # go-excelize-sandbox
+
+
+## Requirements
+
+### Python 3.x
+```
+brew install python3
+pip3 install virtualenv
+python3 -m venv penv
+source penv/bin/activate 
+```
+
+### Install AWS SAM
+
+```
+pip install awscli
+pip install aws-sam-cli
+brew install awscli
+brew install aws-sam-cli
+```
+
+## SetUp SAM
+
+```
+& cd aws-sam-golang
+& make build
+& curl http://localhost:3000/gen
+```
--- main.go
+++ main.go
@@ -1,18 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
-	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
+var (
+	// DefaultHTTPGetAddress Default Address
+	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
+
+	// ErrNoIP No IP found in response
+	ErrNoIP = errors.New("No IP in HTTP response")
+
+	// ErrNon200Response non 200 status code in response
+	ErrNon200Response = errors.New("Non 200 Response found")
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	resp, err := http.Get(DefaultHTTPGetAddress)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if resp.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	}
+
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if len(ip) == 0 {
+		return events.APIGatewayProxyResponse{}, ErrNoIP
+	}
+
 	f := excelize.NewFile()
 	index := f.NewSheet("Sheet1")
 	f.SetCellValue("Sheet1", "B2", 100)
 	f.SetActiveSheet(index)
-	err := f.SaveAs("./Book1.xlsx")
+	err = f.SaveAs("./Book1.xlsx")
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("OK, %v", string(ip)),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
